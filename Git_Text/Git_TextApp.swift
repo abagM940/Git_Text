7bb1060bfca0ea299848c8fce0a2f1338b98a0c9
Abag <abagblacksky@gmail.com>
2023-06-24 14:22:59 +0400

    Initial Commit

---
+++ Git_Text/Git_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TextApp.swift
+//  Git_Text
+//
+//  Created by Abag Magdi  on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
